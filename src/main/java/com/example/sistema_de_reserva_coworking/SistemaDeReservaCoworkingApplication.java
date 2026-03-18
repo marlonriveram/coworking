a30b78fc99808886b8b5020692dbd718fffc05c4
Marlon Mosquera Rivera <marlon.mosquerar@gmail.com>
2026-03-18 09:50:57 -0500

    Se inicializo el proyecto, con la configuración basica de carpetas, se hizo la configuración de swagger y  se realizo la conección a la db

---
+++ src/main/java/com/example/sistema_de_reserva_coworking/SistemaDeReservaCoworkingApplication.java
@@ -0,0 +1,12 @@
+package com.example.sistema_de_reserva_coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservaCoworkingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservaCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_de_reserva_coworking/SistemaDeReservaCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_de_reserva_coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservaCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
